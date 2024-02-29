726e5cbc713a0fcfd495004375c63f5a8eb1bd8a
Yusheng Zhao <yushengzhao2020@outlook.com>
2024-02-29 19:17:41 +0800

    imp alpha conv

--- bin/main.ml
+++ bin/main.ml
@@ -1,31 +1,43 @@
-(* following https://dev.to/chshersh/learn-lambda-calculus-in-10-minutes-with-ocaml-56ba *)
-
-#require "angstrom";;
-open Angstrom
-
 type expr =
     | Var of string
     | App of expr * expr
-    | Lam of string * expr
+    | Lam of char * expr
 
 let rec pretty_print t = match t with
-  | Var x -> x
+  | Var s -> s
   | App (t1, t2) -> "(" ^ (pretty_print t1) ^ " " ^ (pretty_print t2) ^ ")"
-  | Lam (x, t0) -> "λ" ^ x ^ "." ^ (pretty_print t0)
+  | Lam (c, t0) -> "λ" ^ (String.make 1 c) ^ "." ^ (pretty_print t0)
 
-let rec alpha_conversion t x y = match t with
-  | Var z -> Var y
-  | App (t1, t2) -> App (alpha_conversion t1 x y, alpha_conversion t2 x y)
-  | Lam (z, t0) -> if z = x then Lam (z, alpha_conversion t0 x y) else Lam (z, t0)
+let replace_char s old_char new_char =
+  String.map (fun c -> if c = old_char then new_char else c) s
 
-(* let rec beta_reduction t x = match t with
-  | Var y -> if x = y then s else Var y
-  | App (t1, t2) -> App (t1, t2)
-  | Lam (y, t) -> if x = y then Lam (y, t) else Lam (y, beta_reduction t x s) *)
+let rec alpha_conversion_helper t x y= match t with
+  | Var s -> Var (replace_char s x y)
+  | App (_, _) -> t 
+  | Lam (z, t0) -> match z with
+    | x -> Lam (y, alpha_conversion_helper t0 x y)
+    | _ ->  t 
 
-let () = let lc = App (Lam ("x", Var "x"), Var "y") in
-      let lc_alpha = alpha_conversion lc "x" "y" in
+    (* needs to check collision, cannot convert the current var to x if x already exists *)
+let alpha_conversion t x = match t with 
+  | Lam (z, t0) -> Lam(x, alpha_conversion_helper t0 z x)
+  | _ -> t
+
+
+let () = let lc = Lam ('x', Var "xy") in
+      let lc_alpha = alpha_conversion lc 'z' in
       print_endline (pretty_print lc_alpha)
+
+let () = let lc = Lam ('x', Var "y") in
+      let lc_alpha = alpha_conversion lc 'z' in
+      print_endline (pretty_print lc_alpha)
+
+let rec beta_reduction t = match t with
+  | Var y -> Var y
+  | App (t1, t2) -> App (t1, t2)
+  | Lam (y, t0) -> Lam (y, t0) 
+
+
 
 let () = let t = App (Lam ("x", Var "x"), Var "y") in 
           print_endline (pretty_print t)
